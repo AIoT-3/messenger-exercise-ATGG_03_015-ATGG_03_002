feffca8d34ace47c5e9196d21b40b0d2fe04902a
Quasimorphism <bak575755@gmail.com>
2026-01-27 16:20:46 +0900

    공통 예외 추가

---
+++ messenger-common/src/main/java/com/nhnacademy/messenger/common/exception/MessengerException.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.messenger.common.exception;
+
+import com.nhnacademy.messenger.common.payload.common.ErrorCode;
+import lombok.Getter;
+
+public class MessengerException extends RuntimeException {
+
+    @Getter
+    private final ErrorCode errorCode;
+
+    public MessengerException(ErrorCode code) {
+        super(code.name());
+        this.errorCode = code;
+    }
+
+    public MessengerException(ErrorCode code, String message) {
+        super(message);
+        this.errorCode = code;
+    }
+}
--- messenger-common/src/main/java/com/nhnacademy/messenger/common/payload/auth/login/LoginRequest.java
+++ messenger-common/src/main/java/com/nhnacademy/messenger/common/payload/auth/login/LoginRequest.java
@@ -4,4 +4,5 @@ public record LoginRequest(
         String userId,
         String password
 ) {
+
 }
