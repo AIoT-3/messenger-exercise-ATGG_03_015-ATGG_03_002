10261fe7cb71e6a977026e44a89e8b7a78631c89
Quasimorphism <bak575755@gmail.com>
2026-02-02 10:51:42 +0900

    - fix: RequestHeader에 정의된 편의 생성자 사용으로 변경

--- messenger-client/src/main/java/com/nhnacademy/messenger/client/domain/user/service/UserClientService.java
+++ messenger-client/src/main/java/com/nhnacademy/messenger/client/domain/user/service/UserClientService.java
@@ -3,12 +3,9 @@ package com.nhnacademy.messenger.client.domain.user.service;
 import com.nhnacademy.messenger.client.network.MessageClient;
 import com.nhnacademy.messenger.common.message.Message;
 import com.nhnacademy.messenger.common.message.data.auth.LoginRequest;
-import com.nhnacademy.messenger.common.message.header.MessageType;
 import com.nhnacademy.messenger.common.message.header.RequestHeader;
 import com.nhnacademy.messenger.common.util.converter.MessageConverter;
 import lombok.RequiredArgsConstructor;
-
-import java.time.ZonedDateTime;
 
 @RequiredArgsConstructor
 public class UserClientService {
@@ -16,7 +13,7 @@ public class UserClientService {
 
     public void login(String userId, String password) {
         LoginRequest data = new LoginRequest(userId, password);
-        RequestHeader header = new RequestHeader(MessageType.LOGIN, ZonedDateTime.now(), null);
+        RequestHeader header = RequestHeader.login();
         Message message = new Message(header, MessageConverter.objectMapper.valueToTree(data));
         
         messageClient.send(message);
