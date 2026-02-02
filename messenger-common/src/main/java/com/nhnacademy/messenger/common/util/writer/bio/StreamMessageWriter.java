6a82bfee6ac5022dfb40330ef354e9c380734555
Quasimorphism <bak575755@gmail.com>
2026-02-02 10:22:40 +0900

    - fix: synchronized -> ReenterantLock 변경으로 pinned 문제 방지

--- messenger-common/src/main/java/com/nhnacademy/messenger/common/util/writer/bio/StreamMessageWriter.java
+++ messenger-common/src/main/java/com/nhnacademy/messenger/common/util/writer/bio/StreamMessageWriter.java
@@ -8,23 +8,28 @@ import com.nhnacademy.messenger.common.util.writer.MessageWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class StreamMessageWriter implements MessageWriter {
 
     private final DataOutputStream out;
+    private final ReentrantLock lock = new ReentrantLock();
 
     public StreamMessageWriter(OutputStream outputStream) {
         this.out = new DataOutputStream(outputStream);
     }
 
     @Override
-    public synchronized void writeMessage(Message message) {
+    public void writeMessage(Message message) {
+        lock.lock();
         try {
             byte[] bytes = MessageConverter.toBytes(message);
             out.write(bytes);
             out.flush();
         } catch (IOException e) {
             throw new MessageConvertException("메시지 전송 실패", e);
+        } finally {
+            lock.unlock();
         }
     }
 }
