fa126db324b76c161074dd0020305e34d19c2696
Quasimorphism <bak575755@gmail.com>
2026-02-05 10:38:27 +0900

    refactor: 메신저 서버의 MessageType을 static import로 변경

--- messenger-server/src/main/java/com/nhnacademy/messenger/server/network/MessageServer.java
+++ messenger-server/src/main/java/com/nhnacademy/messenger/server/network/MessageServer.java
@@ -1,26 +1,17 @@
 package com.nhnacademy.messenger.server.network;
 
 import com.nhnacademy.messenger.common.event.EventBus;
-import com.nhnacademy.messenger.common.message.header.MessageType;
-import com.nhnacademy.messenger.server.chat.handler.ChatHistoryRequestHandler;
-import com.nhnacademy.messenger.server.chat.handler.ChatRequestHandler;
-import com.nhnacademy.messenger.server.chat.handler.PrivateChatRequestHandler;
-import com.nhnacademy.messenger.server.chat.repository.ChatRepository;
+import com.nhnacademy.messenger.server.chat.handler.*;
 import com.nhnacademy.messenger.server.chat.repository.impl.InMemoryChatRepository;
 import com.nhnacademy.messenger.server.chat.service.ChatService;
 import com.nhnacademy.messenger.server.chat.service.impl.ChatServiceImpl;
-import com.nhnacademy.messenger.server.room.handler.CreateRoomRequestHandler;
-import com.nhnacademy.messenger.server.room.handler.EnterRoomRequestHandler;
-import com.nhnacademy.messenger.server.room.handler.ExitRoomRequestHandler;
-import com.nhnacademy.messenger.server.room.handler.ListRoomRequestHandler;
+import com.nhnacademy.messenger.server.room.handler.*;
 import com.nhnacademy.messenger.server.room.repository.impl.InMemoryChatRoomRepository;
 import com.nhnacademy.messenger.server.room.service.ChatRoomService;
 import com.nhnacademy.messenger.server.room.service.impl.ChatRoomServiceImpl;
 import com.nhnacademy.messenger.server.session.domain.Session;
 import com.nhnacademy.messenger.server.session.manager.SessionManager;
-import com.nhnacademy.messenger.server.user.handler.LoginRequestHandler;
-import com.nhnacademy.messenger.server.user.handler.LogoutRequestHandler;
-import com.nhnacademy.messenger.server.user.handler.UserListRequestHandler;
+import com.nhnacademy.messenger.server.user.handler.*;
 import com.nhnacademy.messenger.server.user.repository.impl.InMemoryUserRepository;
 import com.nhnacademy.messenger.server.user.service.UserService;
 import com.nhnacademy.messenger.server.user.service.impl.UserServiceImpl;
@@ -31,6 +22,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import static com.nhnacademy.messenger.common.config.AppConstant.DEFAULT_SERVER_PORT;
+import static com.nhnacademy.messenger.common.message.header.MessageType.*;
 
 @Slf4j
 public class MessageServer implements Runnable {
@@ -62,16 +54,16 @@ public class MessageServer implements Runnable {
 
         // 2. 디스패처 및 핸들러 초기화
         this.messageDispatcher = new MessageDispatcher();
-        this.messageDispatcher.register(MessageType.LOGIN, new LoginRequestHandler(userService, sessionManager));
-        this.messageDispatcher.register(MessageType.LOGOUT, new LogoutRequestHandler());
-        this.messageDispatcher.register(MessageType.USER_LIST, new UserListRequestHandler(userService, sessionManager));
-        this.messageDispatcher.register(MessageType.CHAT_ROOM_CREATE, new CreateRoomRequestHandler(chatRoomService));
-        this.messageDispatcher.register(MessageType.CHAT_ROOM_LIST, new ListRoomRequestHandler(chatRoomService));
-        this.messageDispatcher.register(MessageType.CHAT_ROOM_ENTER, new EnterRoomRequestHandler(chatRoomService));
-        this.messageDispatcher.register(MessageType.CHAT_ROOM_EXIT, new ExitRoomRequestHandler(chatRoomService));
-        this.messageDispatcher.register(MessageType.CHAT_MESSAGE, new ChatRequestHandler(chatService, chatRoomService));
-        this.messageDispatcher.register(MessageType.PRIVATE_MESSAGE, new PrivateChatRequestHandler(sessionManager));
-        this.messageDispatcher.register(MessageType.CHAT_MESSAGE_HISTORY, new ChatHistoryRequestHandler(userService, chatService, chatRoomService));
+        this.messageDispatcher.register(LOGIN, new LoginRequestHandler(userService, sessionManager));
+        this.messageDispatcher.register(LOGOUT, new LogoutRequestHandler());
+        this.messageDispatcher.register(USER_LIST, new UserListRequestHandler(userService, sessionManager));
+        this.messageDispatcher.register(CHAT_ROOM_CREATE, new CreateRoomRequestHandler(chatRoomService));
+        this.messageDispatcher.register(CHAT_ROOM_LIST, new ListRoomRequestHandler(chatRoomService));
+        this.messageDispatcher.register(CHAT_ROOM_ENTER, new EnterRoomRequestHandler(chatRoomService));
+        this.messageDispatcher.register(CHAT_ROOM_EXIT, new ExitRoomRequestHandler(chatRoomService));
+        this.messageDispatcher.register(CHAT_MESSAGE, new ChatRequestHandler(chatService, chatRoomService));
+        this.messageDispatcher.register(PRIVATE_MESSAGE, new PrivateChatRequestHandler(sessionManager));
+        this.messageDispatcher.register(CHAT_MESSAGE_HISTORY, new ChatHistoryRequestHandler(userService, chatService, chatRoomService));
 
         try {
             this.serverSocket = new ServerSocket(port);
