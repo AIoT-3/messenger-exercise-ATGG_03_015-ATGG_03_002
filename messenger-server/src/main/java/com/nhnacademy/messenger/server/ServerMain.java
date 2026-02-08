339af81471dd266b56a9b14e4b793531a4af81fe
Quasimorphism <bak575755@gmail.com>
2026-02-09 00:40:58 +0900

    feat: queue, nio 구현 완

--- messenger-server/src/main/java/com/nhnacademy/messenger/server/ServerMain.java
+++ messenger-server/src/main/java/com/nhnacademy/messenger/server/ServerMain.java
@@ -1,11 +1,13 @@
 package com.nhnacademy.messenger.server;
 
 import com.nhnacademy.messenger.server.network.MessageServer;
+import com.nhnacademy.messenger.server.network.NioMessageServer;
 
 public class ServerMain {
 
     public static void main(String[] args) {
-        MessageServer messageServer = new MessageServer();
+        // MessageServer messageServer = new MessageServer();
+        NioMessageServer messageServer = new NioMessageServer();
         messageServer.run();
     }
 }
